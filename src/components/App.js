12ec770579d8a319b7dae08586114d7f237c263e
jamesdebnam <jamesdebnam@gmail.com>
2020-06-10 11:18:04 +0100

    Set up react boilerplate

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import master from "../master.css";
+
+import Calculator from "./Calculator";
+export default function App() {
+  return (
+    <div>
+      <h1>Exchange Rate Calculator</h1>
+      <p>This economy eh...</p>
+      <Calculator />
+    </div>
+  );
+}
