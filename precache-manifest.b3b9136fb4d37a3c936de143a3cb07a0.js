b31b1f4a0d8926cc6b1600454f4d6d8af3140064
jamesdebnam <jamesdebnam@gmail.com>
2020-06-12 16:32:19 +0100

    Updates

---
+++ precache-manifest.b3b9136fb4d37a3c936de143a3cb07a0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "713a2ab8e2dacbaa99d458d4f6eb605e",
+    "url": "/exchange-rate-calculator/index.html"
+  },
+  {
+    "revision": "83c9825b7237b47d8d33",
+    "url": "/exchange-rate-calculator/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "e470961a1bc419d90514",
+    "url": "/exchange-rate-calculator/static/css/main.e6f4feee.chunk.css"
+  },
+  {
+    "revision": "83c9825b7237b47d8d33",
+    "url": "/exchange-rate-calculator/static/js/2.487abd4e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/exchange-rate-calculator/static/js/2.487abd4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e470961a1bc419d90514",
+    "url": "/exchange-rate-calculator/static/js/main.1821d470.chunk.js"
+  },
+  {
+    "revision": "ed3bff5f080eab0a902e",
+    "url": "/exchange-rate-calculator/static/js/runtime-main.4b0d7de3.js"
+  },
+  {
+    "revision": "0ed2ecd459fb4ea5c547c4d40e38e3ef",
+    "url": "/exchange-rate-calculator/static/media/right.0ed2ecd4.svg"
+  },
+  {
+    "revision": "8882091a35e5751dec57ce74f6b334f2",
+    "url": "/exchange-rate-calculator/static/media/swap.8882091a.svg"
+  }
+]);
